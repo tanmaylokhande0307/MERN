bb044d3a7552692a0bfbbd8e0a3dcf39b6696ae6
Tanmay Lokhande <tl@zasmlabs.com>
2023-06-19 22:41:51 +0530

    updated the api server

--- Week2/index.js
+++ Week2/index.js
@@ -7,20 +7,23 @@ app.use(bodyParser.json());
 const USERS = [];
 let ids = 0;
 
-const generateIds = (req, res, next) => {
-  const newUser = req.body;
-  newUser["id"] = ids;
-  ids++;
-  USERS.push(newUser);
+const checkIfUserAlreadyExists = (req, res, next) => {
+  const email = req.body.email;
+  const user = USERS.filter((user) => user.email === email);
+  if (user.length > 0) {
+    res.status(200).send("User already exists");
+    return;
+  }
   next();
 };
-
-app.use(generateIds);
 
 const port = 3031;
 
 const createNewUser = (req, res) => {
-  console.log("users", USERS);
+  const newUser = req.body;
+  newUser["id"] = ids;
+  ids++;
+  USERS.push(newUser);
   res.status(200).send("account created successfully");
 };
 
@@ -28,14 +31,25 @@ const getAllUsers = (req, res) => {
   res.status(200).json(USERS);
 };
 
-const showData = (req, res) => {
-  res.send("showing hello world data");
+const loginUser = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = USERS.filter(
+    (user) => user.email === email && user.password === password
+  );
+  if (user.length > 0) {
+    res.status(200).json({ token: Date.now() });
+    return;
+  } else {
+    res.status(200).send("User not found");
+  }
 };
 
-app.post("/signUp", createNewUser);
+app.post("/signUp", checkIfUserAlreadyExists, createNewUser);
 
 app.get("/data", getAllUsers);
 
+app.get("/login", loginUser);
 
 app.listen(port, () => {
   console.log("server is listing on port ", port);
